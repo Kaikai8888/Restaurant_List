cdf6e13eb4ab4d4c93f4d97c215d67f974e920cc
Kaikai8888 <wuky812@gmail.com>
2020-09-27 15:03:45 +0800

    Init: add movie list and templates

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+const restaurants = require('./restaurant.json')
+//setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//setting static files
+app.use(express.static('public'))
+
+//setting routes
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurants.results })
+})
+
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
